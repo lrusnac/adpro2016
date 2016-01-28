561cf338e05d241ac2346da4da2ba789a89f1e5b
Leonid <leonidrusnac4@gmail.com>
2016-01-28 16:35:37 +0100

    added authors

--- 010/solutions/Exercises.scala
+++ 010/solutions/Exercises.scala
@@ -1,7 +1,7 @@
 // Advanced Programming, Exercises by A. Wąsowski, IT University of Copenhagen
 //
-// AUTHOR1:
-// AUTHOR2:
+// AUTHOR1: Leonid Rusnac leru@itu.dk
+// AUTHOR2: Jens Tuxen Johannessen jenj@itu.dk
 //
 // Write names and ITU email addresses of both group members that contributed to
 // the solution of the exercise (in alphabetical order by family name).
