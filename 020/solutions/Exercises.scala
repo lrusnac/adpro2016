0b0df45ed57e0298aa79023c4138f893f01900ba
Jens Tuxen <mail@jenstuxen.com>
2016-02-11 11:27:30 +0100

    pascal plus formatting

--- 020/solutions/Exercises.scala
+++ 020/solutions/Exercises.scala
@@ -32,7 +32,9 @@
 // An ADT of Lists
 
 sealed trait List[+A]
+
 case object Nil extends List[Nothing]
+
 case class Cons[+A](head: A, tail: List[A]) extends List[A]
 
 
@@ -43,27 +45,27 @@ object List {
     else Cons(as.head, apply(as.tail: _*))
 
   // Exercise 2
-  def tail[A] (as: List[A]) :List[A] = as match {
-      case Cons(x, xs) => xs
-      case _ => Nil
+  def tail[A](as: List[A]): List[A] = as match {
+    case Cons(x, xs) => xs
+    case _ => Nil
   }
 
   // Exercise 3
-  def setHead[A] (as: List[A], newHead: A) : List[A] = as match {
+  def setHead[A](as: List[A], newHead: A): List[A] = as match {
     case Cons(x, xs) => Cons(newHead, xs)
     case _ => Nil
   }
 
   // Exercise 4
-  def drop[A] (l: List[A], n: Int) : List[A] = n match {
-    case n if n > 0 => drop(tail(l), n-1)
+  def drop[A](l: List[A], n: Int): List[A] = n match {
+    case n if n > 0 => drop(tail(l), n - 1)
     case _ => l
   }
 
   // Exercise 5
   def dropWhile[A](l: List[A], f: A => Boolean): List[A] = l match {
     case Cons(x, xs) => {
-      if(f(x)) {
+      if (f(x)) {
         dropWhile(xs, f)
       } else {
         l
@@ -93,23 +95,25 @@ object List {
   }
 
   // Exercise 9
-  def foldRight[A,B] (as :List[A], z: B) (f : (A,B)=> B) :B = as match {
+  def foldRight[A, B](as: List[A], z: B)(f: (A, B) => B): B = as match {
     case Nil => z
     case Cons(x, xs) => f(x, foldRight(xs, z)(f))
   }
 
-  def foldLeft[A,B] (as: List[A], z: B) (f: (B, A) => B) : B = as match {
+  def foldLeft[A, B](as: List[A], z: B)(f: (B, A) => B): B = as match {
     case Nil => z
     case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
   }
 
   // Exercise 10
-  def sum (as : List[Int]) : Int = foldLeft(as, 0)(_+_)
-  def product (as :List[Int]) : Int = foldLeft(as, 1)(_*_)
-  def length1 (as :List[Int]) : Int = foldLeft(as, 0)((x, y) => x + 1)
+  def sum(as: List[Int]): Int = foldLeft(as, 0)(_ + _)
+
+  def product(as: List[Int]): Int = foldLeft(as, 1)(_ * _)
+
+  def length1(as: List[Int]): Int = foldLeft(as, 0)((x, y) => x + 1)
 
   // Exercise 11
-  def reverse[A] (as :List[A]) :List[A] = foldLeft(as, List[A]())((xs, x) => Cons(x, xs))
+  def reverse[A](as: List[A]): List[A] = foldLeft(as, List[A]())((xs, x) => Cons(x, xs))
 
   // Exercise 12
   // def foldRight1[A,B] (as: List[A], z: B) (f: (A, B) => B) : B = ...
@@ -119,7 +123,7 @@ object List {
   // Exercise 13
   def append[A](a1: List[A], a2: List[A]): List[A] = a1 match {
     case Nil => a2
-    case Cons(h,t) => Cons(h, append(t, a2))
+    case Cons(h, t) => Cons(h, append(t, a2))
   }
 
   def concat[A](as: List[List[A]]): List[A] = {
@@ -127,54 +131,66 @@ object List {
   }
 
   // Exercise 14
-  def map[A,B](a: List[A]) (f: A => B): List[B] = reverse(foldLeft(a, List[B]())((xs, x) => Cons(f(x), xs)))
+  def map[A, B](a: List[A])(f: A => B): List[B] = reverse(foldLeft(a, List[B]())((xs, x) => Cons(f(x), xs)))
 
   // Exercise 15 (no coding)
 
   // Exercise 16
-  def filter[A] (as: List[A]) (f: A => Boolean) : List[A] = {
+  def filter[A](as: List[A])(f: A => Boolean): List[A] = {
     reverse(foldLeft(as, List[A]())((xs, x) => {
-      if(f(x)) Cons(x, xs)
+      if (f(x)) Cons(x, xs)
       else xs
     }))
   }
 
   // Exercise 17
-  def flatMap[A,B](as: List[A])(f: A => List[B]) : List[B] = foldLeft(as, List[B]())((xs, x) => append(xs, f(x)))
+  def flatMap[A, B](as: List[A])(f: A => List[B]): List[B] = foldLeft(as, List[B]())((xs, x) => append(xs, f(x)))
 
   // Exercise 18
-  def filter1[A] (l: List[A]) (p: A => Boolean) :List[A] = flatMap(l)(x=>{
-    if(p(x)) List(x)
+  def filter1[A](l: List[A])(p: A => Boolean): List[A] = flatMap(l)(x => {
+    if (p(x)) List(x)
     else List()
   })
 
   // Exercise 19
-  def add (l: List[Int]) (r: List[Int]): List[Int] = (l, r) match {
-    case (Cons(x, xs), Cons(y, ys)) => Cons(x+y, add(xs)(ys))
+  def add(l: List[Int])(r: List[Int]): List[Int] = (l, r) match {
+    case (Cons(x, xs), Cons(y, ys)) => Cons(x + y, add(xs)(ys))
     case (_, _) => List[Int]()
   }
 
   // Exercise 20
-  def zipWith[A,B,C] (f : (A,B)=>C) (l: List[A], r: List[B]) : List[C] = (l, r) match {
-    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x,y), zipWith(f)(xs, ys))
+  def zipWith[A, B, C](f: (A, B) => C)(l: List[A], r: List[B]): List[C] = (l, r) match {
+    case (Cons(x, xs), Cons(y, ys)) => Cons(f(x, y), zipWith(f)(xs, ys))
     case (_, _) => List[C]()
   }
 
   // Exercise 21
-  def hasSubsequence[A] (sup: List[A], sub: List[A]) :Boolean = sup match {
-      case Nil => sub == Nil
-      case Cons(x, xs) =>
-          if(hasSubsequenceFromHere(Cons(x, xs), sub) == true) true
-          else hasSubsequence(xs, sub)
+  def hasSubsequence[A](sup: List[A], sub: List[A]): Boolean = sup match {
+    case Nil => sub == Nil
+    case Cons(x, xs) =>
+      if (hasSubsequenceFromHere(Cons(x, xs), sub) == true) true
+      else hasSubsequence(xs, sub)
   }
-  def hasSubsequenceFromHere[A] (sup: List[A], sub: List[A]): Boolean = (sup, sub) match {
-      case (Cons(x, xs), Cons(y, ys)) => x == y && hasSubsequenceFromHere(xs, ys)
-      case (_, Nil) => true
-      case (_, _) => false
+
+  def hasSubsequenceFromHere[A](sup: List[A], sub: List[A]): Boolean = (sup, sub) match {
+    case (Cons(x, xs), Cons(y, ys)) => x == y && hasSubsequenceFromHere(xs, ys)
+    case (_, Nil) => true
+    case (_, _) => false
   }
 
   // Exercise 22
-  // def pascal (n :Int) : List[Int] = ...
+  def pascal(n: Int): List[Int] = {
+
+    def f(l: List[Int]): List[Int] = l match {
+      case Cons(x, Cons(xx, xxs)) => Cons(x + xx, f(Cons(xx, xxs)))
+      case _ => List()
+    }
+
+    n match {
+      case n if n == 1 => List(1)
+      case n if n > 1 => append(Cons(1, f(pascal(n - 1))), List(1))
+    }
+  }
 
   // a test: pascal (4) = Cons(1,Cons(3,Cons(3,Cons(1,Nil))))
 
@@ -186,14 +202,14 @@ object Exercise7 {
 
   case class SalaryLine(name: String, amount: Integer)
 
-  def maximumSalary (salaries: List[SalaryLine]) :Integer = {
+  def maximumSalary(salaries: List[SalaryLine]): Integer = {
     def m(saleries: List[SalaryLine], maximum: Integer): Integer = {
       saleries match {
         case Nil => maximum
-        case Cons(x,xs) => m(xs,Math.max(x.amount,maximum))
+        case Cons(x, xs) => m(xs, Math.max(x.amount, maximum))
       }
     }
-    m(salaries,-1)
+    m(salaries, -1)
   }
 
   //val test_case = List(SalaryLine("John",41),SalaryLine("Alice", 42), SalaryLine("Bob",40))
