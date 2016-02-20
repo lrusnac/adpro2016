c99fad81c479adc381fe8fd2d043bfd751f73b57
Leonid <leonidrusnac4@gmail.com>
2016-02-20 16:51:22 +0100

    some exercises week 4

--- 040/solutions/Main.scala
+++ 040/solutions/Main.scala
@@ -7,14 +7,45 @@ import fpinscala.laziness._
 import fpinscala.laziness.Stream._
 
 // this is how we do simple interactive testing
+object Tests extends App {
 
-val l1 :Stream[Int] = Empty
-val l2 :Stream[Int] = empty
+  val l1: Stream[Int] = Empty
+  val l2: Stream[Int] = empty
 
-val l3 :Stream[Int]= cons(1, cons(2, cons (3, empty)))
+  val l3: Stream[Int] = cons(1, cons(2, cons(3, empty)))
 
-println (l1.headOption)
-println (l2.headOption)
-println (l3.headOption)
+  println(l1.headOption)
+  println(l2.headOption)
+  println(l3.headOption)
 
+  val naturals: Stream[Int] = Stream.from(0)
 
+  println(l3.toList)
+
+  println(Stream.to(10).toList)
+
+  println(naturals.take(1000000000).drop(41).take(10).toList)
+
+  println(naturals.takeWhile(_<1000000000).drop(100).take(50).toList)
+
+  println(naturals.forAll (_ < 0))
+
+  // println(naturals.forAll (_ >=0))
+
+  println(naturals.takeWhileWF(_<1000000000).drop(100).take(50).toList)
+  println(naturals.takeWhileWF(_<1000000000).drop(100).take(50).headOptionWF())
+
+  println(naturals.map(_*2).drop (30).take(50).toList)
+  println(naturals.drop(42).filter(_%2 ==0).take(30).toList)
+  naturals.append(naturals)
+  println(naturals.take(10).append(naturals).take(20).toList)
+
+  //println(naturals.flatMap(a => Stream.to(a)).take(100).toList)
+  //println(naturals.flatMap (x =>from (x)).take (100).toList)
+
+  val fibs = Stream.fibonaci.take(10).toList
+  println(fibs)
+
+  println(empty.fibunfold.take(10).toList)
+  println(naturals.mapunfold(_*2).drop (30).take(50).toList)
+}
--- 040/solutions/Stream.scala
+++ 040/solutions/Stream.scala
@@ -46,7 +46,54 @@ sealed trait Stream[+A] {
       // of ||)
     }
 
-  //def find (p :A => Boolean) :Option[A] = this.filter (p).headOption
+  def find(p :A => Boolean): Option[A] = this.filter(p).headOption
+
+  def toList: List[A] = this match {
+    case Cons(head, tail) => head() :: tail().toList
+    case _ => List()
+  }
+
+  def take(n :Int): Stream[A] = this match {
+    case Cons(h, t) => if(n == 0) Empty else cons(h(), t().take(n-1))
+    case _ => Empty
+  }
+
+  def drop(n :Int): Stream[A] = this match {
+    case Cons(_, t) => if(n > 0) t().drop(n - 1) else this
+    case _ => this
+  }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) => if(p(h())) cons(h(), t().takeWhile(p)) else Empty
+    case _ => Empty
+  }
+
+  def forAll(p: A => Boolean): Boolean = this match {
+    case Cons(h, t) => if(p(h())) t().forAll(p) else false
+    case _ => true
+  }
+
+  def takeWhileWF(p: A => Boolean): Stream[A] = foldRight[Stream[A]](Empty)((a, b) => if(p(a)) cons(a, b) else Empty)
+
+  def headOptionWF(): Option[A] = foldRight[Option[A]](None)((h, _) => Some(h))
+
+  def map[B](f: A => B): Stream[B] = foldRight[Stream[B]](Empty)((a, b) => cons(f(a), b))
+  def filter(f: A => Boolean): Stream[A] = foldRight[Stream[A]](Empty)((a, b) => if(f(a)) cons(a, b) else b)
+  def append[B>:A](s: Stream[B]): Stream[B] = foldRight(s)((a, b) => cons(a, b))
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight[Stream[B]](Empty)((a, b) => f(a).append(b))
+
+
+  def unfold[A, S](z: S)(f: S => Option[(A, S)]): Stream[A] =
+    f(z).map(s => cons(s._1, unfold(s._2)(f))).getOrElse(Empty)
+
+  def fibunfold = unfold((0,1))(s => Some(s._1, (s._2, s._1+s._2)))
+
+  def mapunfold[B](f: A => B): Stream[B] = unfold(this)({
+    case Cons(h, t) => Some(f(h()), t())
+    case _ => None
+  })
+
+  // map, take, takeWhile, zipWith, and zipAll
 }
 
 
@@ -72,6 +119,21 @@ object Stream {
     // Note 1: ":_*" tells Scala to treat a list as multiple params
     // Note 2: pattern matching with :: does not seem to work with Seq, so we
     //         use a generic function API of Seq
+
+  def from(n :Int): Stream[Int] = cons(n, from(n+1))
+  def to(n :Int): Stream[Int] = {
+    def toInternal(current:Int): Stream[Int] = {
+      if(current == n) cons(current, empty)
+      else cons(current, toInternal(current+1))
+    }
+    toInternal(0)
+  }
+
+  def fibonaci: Stream[Int] = {
+    def intfib(a: Int, b: Int): Stream[Int] = cons(a, intfib(b, a + b))
+
+    intfib(0, 1)
+  }
 }
 
 // vim:tw=0:cc=80:nowrap
