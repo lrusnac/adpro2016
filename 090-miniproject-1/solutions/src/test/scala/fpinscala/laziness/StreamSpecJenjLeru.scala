4462dba27783583d98129c0e807dd89ca153c2f2
Leonid <leonidrusnac4@gmail.com>
2016-04-03 14:19:47 +0200

    some work

--- 090-miniproject-1/solutions/src/test/scala/fpinscala/laziness/StreamSpecJenjLeru.scala
+++ 090-miniproject-1/solutions/src/test/scala/fpinscala/laziness/StreamSpecJenjLeru.scala
@@ -14,18 +14,18 @@ import Arbitrary.arbitrary
 // library streams are stricter than those from the book, so some laziness tests
 // fail on them :)
 
-//import stream00._    // uncomment to test the book solution
+import stream00._    // uncomment to test the book solution
 // import stream01._ // uncomment to test the broken headOption implementation
 // import stream02._ // uncomment to test another version that breaks headOption
-import stream03._ // uncomment to test another version that breaks headOption
+// import stream03._ // evaluate tail on headOption
+
+// import stream04._ // evaluates head and tail inside take
 
 class StreamSpecJenjLeru extends FlatSpec with Checkers {
 
   import Stream._
 
   behavior of "headOption"
-
-//  For headOption:
 //  - it should return None on an empty stream;  (already included in the examples)
 //  - it should return some for a non-empty stream;  (already included in the examples)
 //  - headOption should not force the tail of the stream.
@@ -54,9 +54,50 @@ class StreamSpecJenjLeru extends FlatSpec with Checkers {
 
   }
 
-
   it should "not force the tail of the stream" in {
     cons(100, Stream(throw new RuntimeException("it evaluates the tail"))).headOption
   }
+
+  behavior of "take"
+  // - take should not force any heads nor any tails of the Stream it manipulates
+  it should "not force any heads nor any tails of the Stream it manipulates" in {
+    cons(Stream(throw new RuntimeException("it evaluates the head")),
+      Stream(throw new RuntimeException("it evaluates the tail"))).take(1)
+  }
+
+  //  - take(n) does not force (n+1)st head ever (even if we force all elements of take(n))
+
+  //  - s.take(n).take(n) == s.take(n) for any Stream s and any n
+  it should "match all prefix of original stream" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    implicit def arbPositiveInt = Arbitrary[Int] (Gen.choose(0, 100))
+    ("fixed n" |:
+      Prop.forAll{(s :Stream[Int]) => s.take(1).take(1).toList == s.take(1).toList}) &&
+    ("generated n" |:
+      Prop.forAll{(s :Stream[Int], n: Int) => s.take(n).take(n).toList == s.take(n).toList})
+  }
+
+  behavior of "drop"
+//  - s.drop(n).drop(m) == s.drop(n+m) for any n, m (additivity)
+  it should "additivity" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    implicit def arbPositiveInt = Arbitrary[Int] (Gen.choose(0, 100))
+    Prop.forAll{(s :Stream[Int], n:Int, m:Int) => s.drop(n).drop(m).toList == s.drop(n+m).toList}
+  }
+//  - s.drop(n) does not force any of the dropped elements heads
+//  - the above should hold even if we force some stuff in the tail
+
+  behavior of "map"
+//  - x.map(id) == x (where id is the identity function)
+  it should "identity" in check {
+    implicit def arbIntStream = Arbitrary[Stream[Int]] (genNonEmptyStream[Int])
+    Prop.forAll{(s :Stream[Int]) => s.map(m => m).toList == s.toList}
+  }
+//  - map terminates on infinite streams
+
+  behavior of "append"
+//  - propose properties yourself
+
+
 
 }
