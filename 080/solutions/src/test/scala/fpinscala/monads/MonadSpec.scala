5a3c8a449289ae3bf21025eb68ec6650c9d8dc7a
Jens Tuxen <mail@jenstuxen.com>
2016-04-10 19:52:39 +0200

    monad stuff

--- 080/solutions/src/test/scala/fpinscala/monads/MonadSpec.scala
+++ 080/solutions/src/test/scala/fpinscala/monads/MonadSpec.scala
@@ -6,11 +6,11 @@
 // https://github.com/rickynils/scalacheck/wiki/User-Guide
 
 package fpinscala.monads
-import org.scalacheck._
+import fpinscala.monads.Monad._
 import org.scalacheck.Prop._
-import Arbitrary.arbitrary
+import org.scalacheck._
+
 import scala.language.higherKinds
-import Monad._
 
 
 object  MonadSpec extends Properties("Monad[F[_]] laws..") {
@@ -35,13 +35,15 @@ object  MonadSpec extends Properties("Monad[F[_]] laws..") {
     associative[A,F] (m) && identity[A,F] (m)
 
   // uncomment when you have optionMonad
-  // property ("of optionMonad") = monad[Int,Option] (optionMonad)
+  property ("of optionMonad") = monad[Int,Option] (optionMonad)
 
   // Exercise 17
 
-  // property ...
-  // property ...
-  // property ...
+  //Then add test properties for list monad over integers, stream monad of integers, and stream monad
+  //over strings (in the MonadSpec.scala file).
+  property ("of listMonad") = monad[Int,List] (listMonad)
+  property ("of streamMonad") = monad[Int,Stream](streamMonad)
+  property ("of streamMonad on Strings") = monad[String,Stream](streamMonad)
 
   // Exercise 19
 
